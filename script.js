f426f895db1ccbf6d6d625dc8f67246b15ec7a86
Billy Ardhana <140936796+ardnbilly@users.noreply.github.com>
2024-01-02 22:01:38 +0700

    Eliminate code duplication

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 function getBMR(weight, height, age, gen) {
   let bmr = '';
+  const maleFormula = 66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age);
+  const femaleFormula = 655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age);
   if (gen === 'male') {
-    bmr = 66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age);
-    return bmr;
+    bmr = maleFormula;
   } else {
-    bmr = 655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age);
-    return bmr;
+    bmr = femaleFormula
     }
+    return bmr;
   }
 
-function getAMR(activity, bmr) {
-  if (activity === 'sedentary') {
-    return bmr * 1.2
-  } else if (activity === 'light') {
-    return bmr * 1.375
-  } else if (activity === 'moderate') {
-    return bmr * 1.55
-  } else if (activity === 'active') {
-    return bmr * 1.725
-  } else {
-    return bmr * 1.9
+function getActivityModifier(activity) {
+  switch (activity) {
+    case 'sedentary':
+      return 1.2;
+    case 'light':
+      return 1.375;
+    case 'moderate':
+      return 1.55;
+    case 'active':
+      return 1.725;
+    default:
+      return 1.9;
   }
+}
+
+function getAMR(activity, bmr) {
+  const activityModifier = getActivityModifier(activity);
+  return bmr * activityModifier;
+}
+
+function getInputUser() {
+  const age = document.getElementById('age').value;
+  const height = document.getElementById('height').value;
+  const weight = document.getElementById('weight').value;
+  const activity = document.querySelector('.activity').value;
+  const gender = document.querySelector('input[name="gender"]:checked').value;
+
+  return { age, height, weight, activity, gender };
 }
 
 const checkButton = document.getElementById('check');
@@ -30,12 +47,7 @@ const p = div.querySelector('p')
 
 // get result calculation
 checkButton.addEventListener('click', (elem) => {
-
-  const age = document.getElementById('age').value;
-  const height = document.getElementById('height').value;
-  const weight = document.getElementById('weight').value;
-  const activity = document.querySelector('.activity').value;
-  const gender = document.querySelector('input[name="gender"]:checked').value;
+  const { age, height, weight, activity, gender } = getInputUser();
 
   const bmr = getBMR(weight, height, age, gender);
   const result = getAMR(activity, bmr);
